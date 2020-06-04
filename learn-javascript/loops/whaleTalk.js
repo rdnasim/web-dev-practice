a2688209a0d92d22fe1349ed05d902ef3ea69353
Riadul Islam Nasim <rdislamnasim78@gmail.com>
2020-06-04 09:14:36 +0600

    loops whaleTalk.js

---
+++ learn-javascript/loops/whaleTalk.js
@@ -0,0 +1,16 @@
+let input = 'whale talk';
+input = input.toLowerCase();
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+        if (input[inputIndex] === vowels[vowelIndex]) {
+            resultArray.push(input[inputIndex]);
+        }
+    }
+    if (input[inputIndex] === 'e' || input[inputIndex] === 'u') {
+        resultArray.push(input[inputIndex]);
+    }
+}
+console.log(resultArray.join(' ').toUpperCase());
