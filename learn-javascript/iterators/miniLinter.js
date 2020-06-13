18e3732126d4bfdf7c15c183c1dfc95f5f3317c1
Riadul Islam Nasim <rdislamnasim78@gmail.com>
2020-06-13 11:25:08 +0600

    update

--- learn-javascript/iterators/miniLinter.js
+++ learn-javascript/iterators/miniLinter.js
@@ -8,11 +8,15 @@ let storyWords = story.split(" ");
 
 console.log("The original story contains " + storyWords + " words\n");
 
-let betterWords = storyWords.filter(word => !unnecessaryWords.includes(word));
+let betterWords = storyWords.filter((word) => !unnecessaryWords.includes(word));
 
-console.log('The story contains ' + betterWords.length + ' words after removing unnecessary words\n');
+console.log(
+  "The story contains " +
+  betterWords.length +
+  " words after removing unnecessary words\n"
+);
 
-overusedWords.forEach(function(overWord){
+overusedWords.forEach(function (overWord) {
   let overWordCount = 0;
   betterWords.filter(function (storyWord) {
     if (overWord === storyWord) {
@@ -20,9 +24,146 @@ overusedWords.forEach(function(overWord){
     }
   });
   if (overWordCount != 1) {
-    console.log('The word ' + overWord.toLocaleUpperCase() + ' was used ' + overWordCount + ' times\n');
-  }
-  else{
-    console.log('The Word ' + overWord.toUpperCase() + ' was used ' + overWordCount + ' time\n');
+    console.log(
+      "The word " +
+      overWord.toLocaleUpperCase() +
+      " was used " +
+      overWordCount +
+      " times\n"
+    );
+  } else {
+    console.log(
+      "The Word " +
+      overWord.toUpperCase() +
+      " was used " +
+      overWordCount +
+      " time\n"
+    );
   }
 });
+
+/// count sentence
+let sentenceCount = 0;
+betterWords.forEach(word => {
+  if (word[word.length-1] === '.' || word[word.length-1] === '!') {
+    sentenceCount++;
+  }
+});
+console.log('There are ' + (sentenceCount + 1) + ' sentences in the story\n');
+
+/// log the refined story
+console.log(betterWords.join(' ') + '\n');
+
+///REMOVE AND REPLACE EVERY OTHER INSTANCE OF OVER USED WORDS
+console.log('-------------------------------------------------------');
+console.log('\nProject: Remove and replace every other instance of overused words!\n\n');
+
+/// Create an array to hold a count for each item in 'overusedWords'
+let overusedWC = overusedWords.map(function(){
+  return 0;
+});
+
+///Initiate a count for removed words
+let removedWC = 0;
+
+
+///filter out every other instance of the contents of 'overUsedWords'
+
+let lessOverusedWords = betterWords.map(function(storyWord){
+  if (!overusedWords.includes(storyWord)) {
+    return storyWord;
+  }
+  else{
+    let pos = overusedWords.indexOf(storyWord);
+    if(overusedWC[pos] < 1){
+      overusedWC[pos]++;
+      return storyWord;
+    }
+    else{ /// add alternative words
+      overusedWC[pos] = 0;
+      removedWC++;
+      /// here we could pull from an array of alternative words
+      return 'REMOVED';
+    }
+  }
+});
+
+/// log results
+
+console.log(lessOverusedWords.join(' ') + '\n');
+console.log(removedWC + ' overused words were removed \n');
+
+overusedWords.forEach(function (overWord) {
+  let overWordCount = 0;
+  lessOverusedWords.filter(function (storyWord) {
+    if(overWord === storyWord){
+      overWordCount++;
+    }
+  });
+  if(overWordCount != 1){
+    console.log('The word ' + overWord.toUpperCase() + ' was used ' + overWordCount + ' times\n');
+  }
+  else{
+    console.log('The word ' + overWord.toUpperCase() + ' was used ' + overWordCount + ' time\n');
+  }
+});
+
+////// FIND THE WORD THAT APEARS THE GREATEST NUMBER OF TIMES
+console.log('---------------------------------------------------');
+console.log('\nProject: Find the word that appears the greatest number of time! \n\n');
+
+// Remove puntuation and capitalisation
+
+/// Create an array of accepted characters to be used to filter out punctuation
+const acceptedChars = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+/// Replace capitals with lower case
+let noCaps = story.toLowerCase();
+
+/// Create an array of individual characters
+let noCapsChars = noCaps.split('');
+
+/// Filter out punctuation characters
+let noPunctChars = noCapsChars.filter(char => acceptedChars.includes(char));
+
+/// Recreate story with no capitals or punctuation
+let noPunctStory = noPunctChars.join('');
+
+/// Replace double spaces with single spaces to eliminate blank entries in word arrays
+let noPunctSentences = noPunctStory.split('  ');
+let noDoubleSpaces = noPunctSentences.join(' ');
+
+/// Create an array of story words
+let noPunctWords = noDoubleSpaces.split(' ');
+
+/// Create an array of individual words
+let noDupes = [];
+noPunctWords.forEach(function(word) {
+  if (!noDupes.includes(word)) {
+    noDupes.push(word);
+  }
+});
+
+/// Count how many instances of each individual word
+let instanceCount =[];
+noDupes.forEach(function(countWord) {
+  let count = 0;
+  noPunctWords.forEach(function(storyWord) {
+    if (countWord === storyWord) {
+      count++;
+    }
+  });
+  /// Create a 2D array to store each word and its count
+  instanceCount.push([count, countWord]);
+});
+
+/// Sort the array by decending word count
+instanceCount.sort(function(a, b) {
+  return b[0] - a[0];
+});
+
+/// Log the most used words
+console.log('The Most Used Words. \n'.toUpperCase())
+for (let i = 0; instanceCount[i][0] > 3; i++) {
+  console.log('The word ' + instanceCount[i][1].toUpperCase() + ' was used ' + instanceCount[i][0] + ' times')
+}
